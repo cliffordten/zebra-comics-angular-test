242f2bd48e02e8687e0978f313d0779b59b39b62
cliffordten <cliffordteng5@gmail.com>
2024-10-18 06:11:10 +0100

    added synopsis page

---
+++ src/app/pages/synopsis/synopsis.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-synopsis',
+  standalone: true,
+  imports: [],
+  templateUrl: './synopsis.component.html',
+  styleUrl: './synopsis.component.scss'
+})
+export class SynopsisComponent {
+
+}
