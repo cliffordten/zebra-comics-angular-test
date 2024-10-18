d2419e5ee9253db60dbda56c9998ea944ff2cb24
cliffordten <cliffordteng5@gmail.com>
2024-10-18 02:07:51 +0100

    added sample data for books

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HeroComponent } from '../../components/hero/hero.component';
 import { SectionContainerComponent } from '../../components/section-container/section-container.component';
 import { BookItemComponent } from '../../components/book-item/book-item.component';
+import { IBook } from '../../models/books';
 
 @Component({
   selector: 'app-home',
@@ -10,4 +11,49 @@ import { BookItemComponent } from '../../components/book-item/book-item.componen
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  books: IBook[] = [
+    {
+      title: 'Touch and Follow Iron man',
+      currentEpisode: 'Episode 7',
+      cover: '/book_1.png',
+      progressRead: 10,
+    },
+    {
+      title: 'Kepnee and Tanka',
+      currentEpisode: 'Episode 1',
+      cover: '/book_2.png',
+      progressRead: 30,
+    },
+    {
+      title: 'Minuu and the Five Lions',
+      currentEpisode: 'Episode 3',
+      cover: '/book_3.png',
+      progressRead: 5,
+    },
+    {
+      title: 'Ten of Hearts',
+      currentEpisode: 'Episode 1',
+      cover: '/book_4.png',
+      progressRead: 55,
+    },
+    {
+      title: 'Kepnee and the Crocodile',
+      currentEpisode: 'Episode 4',
+      cover: '/book_1.png',
+      progressRead: 25,
+    },
+    {
+      title: 'Super Soldier',
+      currentEpisode: 'Episode 15',
+      cover: '/book_2.png',
+      progressRead: 60,
+    },
+    {
+      title: 'The Walking Dead',
+      currentEpisode: 'Episode 8',
+      cover: '/book_3.png',
+      progressRead: 10,
+    },
+  ];
+}
